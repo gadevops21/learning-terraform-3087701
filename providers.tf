d388b154f6c97164b958a193b4ac1103124bdea4
gadevops21 <94267093+gadevops21@users.noreply.github.com>
2023-02-04 20:48:10 -0500

    first commit

--- providers.tf
+++ providers.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "us-east-1"
 }
